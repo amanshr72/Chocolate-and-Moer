2d40c67b40b78680afa12ba9c548ac1266727cc0
[Aman <amansh2786@gmail.com>
2022-10-19 13:38:46 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,257 @@
+<?php
+require "header.php";
+?>
+
+<header class="header">
+    <div class="row">
+        <div class="col-md-12 text-center">
+   <a class="logo"><img src="img/logo1.png" alt="logo"></a>
+   </div>
+        <div class="col-md-12 text-center">
+            <button type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-light btn-lg"><em>Make a Reservation Now!</em></button>
+        </div>
+    </div>
+</header>
+
+
+
+<!--about us section-->
+
+<section id="aboutus">
+
+ <div class="container">
+   <h3 class="text-center"><br><br>Chocolate & Moer</h3>
+   <div class="row">
+       <!-- about us -->
+       <!--carousel-->
+     <div class="col-sm"><br><br>
+      	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+         <ol class="carousel-indicators">
+           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+         </ol>
+        <div class="carousel-inner">
+           <div class="carousel-item active">
+             <img class="d-block w-100" src="img/3.jpeg" alt="First slide" style="border-radius: 20px; ">
+           </div>
+           <div class="carousel-item">
+           <img class="d-block w-100" src="img/4.jpeg" alt="Second slide" style="border-radius: 20px; ">
+           </div>
+           <div class="carousel-item">
+           <img class="d-block w-100" src="img/5.jpeg" alt="Third slide" style="border-radius: 20px; ">
+           </div>
+        </div>
+         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Previous</span>
+         </a>
+         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Next</span>
+         </a>
+       </div><br><br>
+     </div>
+<!--end of carousel-->
+  
+         <div class="col-sm">
+    	<div class="arranging"><br><hr>
+	<h4 class="text-center">Our Story</h4>
+	<p><br>The restaurant Chocolate & Moer, first opened in 2004 in <strong> Bhopal </strong>, one of the oldest districts of Athens in the historical center of the city. In 2010 , the restaurant was awarded its first Michelin star and has retained it since.<br><br>
+	The same year, the restaurant relocated to the 6th floor of the Onassis Cultural Centre in Syngrou Bhopal Avenue and was recently redesigned by award-winning architectural firm Divercity.In warmer months of the year the restaurant moves
+	to the 7th floor of the building, offering a unique outdoor setting with panoramic view of the Acropolis, Lycebettus hill and the city skyline.<br><br><br></p><hr>
+	</div>
+     </div>
+    </div>
+
+<!-- about us end -->    
+
+<!-- our chef -->
+<div class="row">
+     <div class="col-sm">
+    	<div class="arranging"><br><hr>
+	<h4 class="text-center">Our Chef</h4>
+	<p>Vikas Khanna <br> Vikas Khanna has certainly made a name for himself, not just in India, but even internationally. He is a Michelin star chef and his restaurant Junoon has won Michelin stars for 6 consecutive years! In case you still don’t recognise him, he was the host of the MasterChef India Season 2. A great chef and an equally nice person, his passion for culinary art is enough to make you want to start cooking!<br></p><hr>
+	<p> Vicky Ratnani <br> He won the Best Chef Of The Year in 2015 and is known for rolling out some great recipes that are not just easy to follow but the end results look spectacular. He has also been featured on NDTV for his show Vicky Goes Veg, wherein he taught masses about some delicious recipes that one could cook up at home! <br></p><hr>
+	<p> Pankaj Bhadouria <br> A renowned name in the Indian culinary world, she is the winner of MasterChef India Season 1. Since then she has been spreading her love for cooking by teaching people recipes through several shows like ‘Chef Pankaj Ka Zayka’ and ‘3 Course with Pankaj’, to name a few. <br><br></p><hr>
+	</div>
+     </div><div class="col-sm"><br><br>
+      	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+         <ol class="carousel-indicators">
+           <li data-target="#carouselExampleIndicators" data-slide-to="0" class=""></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li>
+         </ol>
+        <div class="carousel-inner">
+           <div class="carousel-item">
+             <img class="d-block w-100" src="img/9.jpeg" alt="First slide" style="border-radius: 30px; ">
+           </div>
+           <div class="carousel-item active">
+           <img class="d-block w-100" src="img/10.jpeg" alt="Second slide" style="border-radius: 30px; ">
+           </div>
+           <div class="carousel-item">
+           <img class="d-block w-100" src="img/11.jpeg" alt="Third slide" style="border-radius: 30px; ">
+           </div>
+        </div>
+         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Previous</span>
+         </a>
+         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Next</span>
+         </a>
+       </div><br><br>
+     </div>     
+    </div>
+    <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="social-icon">
+        <a href="facebook.com"><i class="fa fa-facebook"></i></a>
+        <a href="instagram"><i class="fa fa-instagram"></i></a>
+        <a href="twitter.com"><i class="fa fa-twitter"></i></a>
+        <a href="youtube.com"><i class="fa fa-youtube"></i></a>
+        <a href="pintrest.com"><i class="fa fa-pinterest"></i></a>
+        <a href="gmail.com"><i class="fa fa-envelope"></i></a>
+      </div>
+      
+    </div>
+  </div>
+<!-- our chef end -->
+
+    <br>
+  </div>
+</section>
+<!--end of about us section-->
+
+<div class="header2">
+</div>
+
+<!----gallery -->
+<div class id="gallery"><br>
+    <div class="container">
+    <h3 class="text-center"><br>Gallery<br><br></h3>
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+           <div class="d-flex flex-column">
+              <img src="img/1.jpg" class="img-fluid">
+              <img src="img/2.png" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+              <img src="img/3.jpeg" class="img-fluid">
+              <img src="img/4.jpeg" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+               <img src="img/5.jpeg" class="img-fluid">
+               <img src="img/6.jpeg" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+               <img src="img/7.jpeg" class="img-fluid">
+               <img src="img/8.jpeg" class="img-fluid">
+           </div>
+        </div>
+    </div>
+</div><br><br>
+<!----end of gallery -->
+
+<div class="container" id="reservation">
+    <h3 class="text-center"><br><br>Reservation<br><br></h3>
+    <img  src="img/16.jpg" class="img-fluid rounded">
+    <button type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-dark btn-block btn-lg">Make a reservation Now!</button>
+        
+</div><br><br>
+
+<div class="header2">
+</div>
+
+<!-- main page map section-->
+<section class="map" id="footer">
+    <div class="container">
+    <h3 class="text-center"><br><br>Find us!</h3><br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234700.85820748063!2d77.26580556290222!3d23.19934769320925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c428f8fd68fbd%3A0x2155716d572d4f8!2sBhopal%2C%20Madhya%20Pradesh!5e0!3m2!1sen!2sin!4v1637062972671!5m2!1sen!2sin" style= "width:100%;  height:250px; border:0;" allowfullscreen></iframe>
+    
+        <div class="row staff">
+            <div class="col">
+            <h4><strong>Opening Hours</strong></h4>
+                       
+                <div class="signup-form">
+                    <form action="#footer" method="post">
+                        <div class="form-group">
+                            <label>Enter Date</label>
+                            <input type="date" class="form-control" name="date" placeholder="Date" required="required">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="check_schedule" class="btn btn-dark btn-block">Check Opening Time</button>
+                        </div>
+                    </form>
+                    
+<?php
+
+if (isset($_POST['check_schedule'])) {
+    require 'includes/dbh.inc.php';
+            
+    $date= $_POST['date'];
+ 
+    $sql = "SELECT * FROM schedule WHERE date = '$date'";
+    $result = $conn->query($sql);
+    if ($result->num_rows == 1) {
+        while ($row = $result->fetch_assoc()) {
+            echo"
+                <table class='table table-sm table-striped table-dark text-center'>
+                   <thead>
+                    <tr>
+                    <th scope='col'>Date</th>
+                    <th scope='col'>Open Time</th>
+                    <th scope='col'>Close Time</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                    <th scope='row'><em>". $date . "</em></th>
+                    <td>".$row['open_time']."</td>
+                    <td>".$row['close_time']."</td>
+                    </tr>
+                   </tbody>
+                </table>";
+        }
+    } else {
+        echo"
+                <table class='table table-striped table-dark text-center'>
+                   <thead>
+                    <tr>
+                    <th scope='col'>Date</th>
+                    <th scope='col'>Open Time</th>
+                    <th scope='col'>Close Time</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                    <th scope='row'><em>". $date . "</em></th>
+                    <td>12:00</td>
+                    <td>00:00</td>
+                    </tr>
+                   </tbody>
+                </table>";
+    }
+         
+    //close connection
+    mysqli_close($conn);
+}
+?>
+                        
+                </div><br>
+            </div>
+
+            <div class="col">
+            <h4 class="text-right"><strong>Visit Us</strong></h4>
+            <p class="text-right">Chocolate & Moer<br><i class="fa fa-map-marker"></i>&nbsp; Leof. Posidonos 109, <br>Bhopal <br><br><i class="fa fa-envelope-o"></i> info@chocolateandmoer.com <br><i class="fa fa-phone"></i> +91 9988776655</p>
+            </div>
+
+	</div>
+    </div>
+</section>
+<!--end of main page map section-->
+
+
+<?php
+require "footer.php";
+?>
